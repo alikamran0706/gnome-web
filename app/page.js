6b9756643bb9bd225e689b3a0187009d22b16331
ali.kamran <ali.kamran@clvtechnologies.com>
2024-11-30 14:31:04 +0500

    without window

--- app/page.js
+++ app/page.js
@@ -71,48 +71,7 @@ export default function Home() {
     const textRef = useRef(null);
 
     useEffect(() => {
-        if (typeof window !== "undefined" && typeof window !== undefined) {
-          const textSpans = textRef.current.querySelectorAll("span");
-    
-          const handleMouseMove = (event) => {
-            const { clientX, clientY } = event;
-    
-            textSpans.forEach((span) => {
-              const spanRect = span.getBoundingClientRect();
-              const spanCenterX = spanRect.left + spanRect.width / 2;
-              const spanCenterY = spanRect.top + spanRect.height / 2;
-    
-              const distance = Math.sqrt(
-                Math.pow(clientX - spanCenterX, 2) + Math.pow(clientY - spanCenterY, 2)
-              );
-    
-              // Animate spans within a certain distance
-              if (distance < 100) {
-                gsap.to(span, {
-                  scale: 1.5,
-                  color: "#00f2ff",
-                  textShadow: "0px 0px 10px rgba(0, 242, 255, 0.8)",
-                  duration: 0.4,
-                  ease: "power3.out",
-                });
-              } else {
-                gsap.to(span, {
-                  scale: 1,
-                  color: "white",
-                  textShadow: "none",
-                  duration: 0.4,
-                  ease: "power3.out",
-                });
-              }
-            });
-          };
-    
-          window.addEventListener("mousemove", handleMouseMove);
-    
-          return () => {
-            window.removeEventListener("mousemove", handleMouseMove);
-          };
-        }
+       
       }, []);
     
 
